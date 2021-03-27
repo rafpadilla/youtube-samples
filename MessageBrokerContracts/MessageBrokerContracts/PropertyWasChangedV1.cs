8be1b4ac8066a6a277c7dcc3aad2db444f9dc4cc
Rafael Padilla <rafpadilla@gmail.com>
2021-03-27 15:05:43 +0100

    Initial

---
+++ MessageBrokerContracts/MessageBrokerContracts/PropertyWasChangedV1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MessageBrokerContracts
+{
+    /// <summary>
+    /// Message for broker when Property changed
+    /// </summary>
+    public class PropertyWasCreatedV1
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the property.
+        /// </summary>
+        /// <value>
+        /// The name of the property.
+        /// </value>
+        public string PropertyName { get; set; }
+        /// <summary>
+        /// Gets or sets the created.
+        /// </summary>
+        /// <value>
+        /// The created.
+        /// </value>
+        public DateTimeOffset Created { get; set; }
+    }
+}
